07631de005fa1c3bf0d29b46f647b6749752d0c1
szaamerik <szaamerik@outlook.com>
2023-07-28 20:46:23 +0200

    pointers and shit

--- ForzaMods_CarTable/MainWindow.xaml.cs
+++ ForzaMods_CarTable/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using ForzaMods_CarTable.Resources;
 using Memory;
 using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Diagnostics;
 using System.Globalization;
 using System.Text.RegularExpressions;
@@ -13,11 +13,13 @@ using System.Windows.Input;
 namespace ForzaMods_CarTable
 {
     public partial class MainWindow : Window
-    {
+{
+        // variables
         public Mem M = new Mem();
         public static MainWindow mw;
         private bool Attached = false;
-        private IEnumerable<nuint> Addresses = null;
+        private string BaseAddress = null;
+        private string Address = null;
         public bool IsGetIdsOpen = false;
         public int Times_Clicked = 0;
 
@@ -25,11 +27,13 @@ namespace ForzaMods_CarTable
         {
             InitializeComponent();
             mw = this;
+            // setting the culture helped some people scan in the older versions, idk why ??
             CultureInfo.CurrentCulture = new CultureInfo("en-GB");
             Task.Run(ForzaAttach);
         }
 
-        void ForzaAttach()
+        // main attach thread
+        async void ForzaAttach()
         {
             while (true)
             {
@@ -41,6 +45,77 @@ namespace ForzaMods_CarTable
 
                     Attached = true;
                     UpdateUI("Opened Forza Process", true);
+                    
+                    // more variables such as where to start scan, aob string ect
+                    Process Process = Process.GetProcessesByName("ForzaHorizon5")[0];
+                    long start = (long)Process.MainModule.BaseAddress;
+                    long end = (long)(Process.MainModule.BaseAddress + Process.MainModule.ModuleMemorySize);
+                    string aob = null;
+                    string testaddr = "0";
+                    bool ingame = false;
+                    nuint add = 0;
+
+
+                    // defines what to add and the aob
+                    if (Process.MainModule.FileName.Contains("Microsoft.624F8B84B80"))
+                    {
+                        aob = "00 00 78 33 00 80 00 00 00 00 80 A9 ?? ?? ?? ?? 00 00 80 21 ?? ?? ?? 7F 00";
+                        add = 0x2a;
+                    }
+                    else
+                    {
+                        aob = "00 90 25 ?? ?? ?? ? 00 00 ?? ?? ?? ?? ?? ? 00 00 FF FF FF FF 00 00 00 00 00";
+                        add = 0x1;
+                    }
+                        
+
+                    // test address is the car id address
+                    while (true)
+                    {
+                        if (testaddr == "0" || testaddr == "2A")
+                        {
+                            testaddr = ((await M.AoBScan(start, end, "00 00 50 4C 41 59 45 52 5F 43 41 52 00 00 00 00 00 00 0A", true, true, false)).FirstOrDefault() + 0x2A).ToString("X");
+                            UpdateUI("Waiting for testaddr", true);
+                        }
+                        else if (!M.OpenProcess("ForzaHorizon5"))
+                            break;
+                        else
+                            break;
+
+                        Thread.Sleep(50);
+                        
+                    }
+                        
+
+                    // checks if player is ingame
+                    // used as reading car id and checking if its not 0
+                    while (true)
+                    {
+                        if (M.ReadMemory<int>(testaddr) != 0 || !M.OpenProcess("ForzaHorizon5"))
+                            break;
+                        else
+                            UpdateUI("Not ingame, cant scan", true);
+
+                        Thread.Sleep(500);
+                    }
+
+
+                    // base address for pointers
+                    while (BaseAddress == "0" || BaseAddress == null || BaseAddress == "2a" || BaseAddress == "1")
+                       BaseAddress = ((await M.AoBScan(start, end, aob, true, true)).FirstOrDefault() + add).ToString("X");
+
+                    try
+                    {
+                        // pointers
+                        if (Process.MainModule.FileName.Contains("Microsoft.624F8B84B80"))
+                            Address = (BaseAddress + ",0x88,0x78,0x50,0x420,0x20,0x38,0x88,0x60,0x68,0x58,0x98,0x58,0x20,0x8F0,0x648");
+                        else
+                            Address = (BaseAddress + ",0xF0,0x30,0x80,0x10,0x60,0xA0,0x60,0x98,0x58,0x20,0x8F0,0x648");
+
+                        UpdateUI("Scanned for Viper ID", true);
+
+                    } catch { continue; }
+
                     UpdateUI(attached: true);
                 }
                 else
@@ -54,73 +129,41 @@ namespace ForzaMods_CarTable
                 }
             }
         }
-
+        
+        // move the window
         private void Topbar_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
                 DragMove();
         }
 
+
+        // closing
         private void Close_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
+            {
                 Environment.Exit(0);
-        }
 
-        private void Swap_Click(object sender, RoutedEventArgs e)
-        {
-            if (Regex.IsMatch(ID_Box.Text, @"^[0-9]+$") && Addresses != null)
-            {
-                if (int.TryParse(ID_Box.Text, out int writeValue) && Addresses != null)
-                {
-                    foreach (nuint addr in Addresses)
-                        M.WriteMemory<int>(addr.ToString("X"), writeValue);
-                }
-
-                UpdateUI("Swapped the ID", true);
-
-                if ((bool)MessageboxSwitch.IsChecked)
-                    MessageBox.Show("Please verify if the price changed, if not buy at your own risk. We currently do not guarantee that it works 100%", "Warning");
+                if (Attached)
+                    M.WriteMemory<int>(Address, 3003);
             }
-            else if (Addresses == null)
-                MessageBox.Show("Scan first silly");
-            else
-                MessageBox.Show("Input accepts only numbers");
         }
 
-        private async void Scan_Click(object sender, RoutedEventArgs e)
-        {
-            UpdateUI("Scanning for FD Viper Address", true);
-            Addresses = null;
-
-            Thread ScanThread = new Thread(async () =>
-            {
-                try
-                {
-                    Addresses = await M.AoBScan("BB 0B 00 00 00 00 00 00 02 00 00 00 00 00 00 00 00", true, true, false);
-                }
-                catch { }
-
-                if (Addresses != null)
-                    UpdateUI("Found the FD Viper Address", true);
-                else
-                    UpdateUI("Failed at finding the address", true);
-            });
-            ScanThread.Start();
-        }
+        // open car id list
         private void List_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
                 Process.Start("explorer.exe", "https://github.com/ForzaMods/fh5idlist");
         }
 
+        // updating ui, status and shit
+        // this is like ultra shit code and I hate what Ive done but it works so I dont really care
         void UpdateUI(string text = "", bool status = false, bool attached = false)
         {
             if (!status)
                 Dispatcher.BeginInvoke((Action)delegate ()
                 {
-                    Swap.IsEnabled = attached;
-                    Scan.IsEnabled = attached;
                     GetIds.IsEnabled = attached;
                     ID_Box.IsEnabled = attached;
                 });
@@ -134,6 +177,7 @@ namespace ForzaMods_CarTable
 
         private void GetIds_Click(object sender, RoutedEventArgs e)
         {
+            // opens id window
             if (!IsGetIdsOpen)
             {
                 var getids = new CarIds();
@@ -142,10 +186,33 @@ namespace ForzaMods_CarTable
                 Times_Clicked = 0;
             }
 
+            // easter egg
             if (Times_Clicked > 5)
                 MessageBox.Show("Stop spamming this button retard");
 
             Times_Clicked++;
+        }
+
+        // mem writing
+        private void ID_Box_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            // checking if correct and if so then write to the address
+            if (Regex.IsMatch(ID_Box.Text, @"^[0-9]+$") && (BaseAddress != "0" || BaseAddress != null || BaseAddress != "2a" || BaseAddress != "1"))
+            {
+                if (int.TryParse(ID_Box.Text, out int writeValue))
+                    M.WriteMemory<int>(Address, writeValue);
+
+                UpdateUI("Swapped the ID", true);
+            }
+            // checking for base address
+            else if (BaseAddress == "0" || BaseAddress == null || BaseAddress == "2a" || BaseAddress == "1")
+                MessageBox.Show("Base address for the pointer is incorrect. Please restart your game and tool", "Baseaddress error");
+            // error if not correct
+            else
+            {
+                MessageBox.Show("Input accepts only numbers");
+                M.WriteMemory<int>(Address, 3003);
+            }
         }
     }
 }
--- ForzaMods_CarTable/Resources/CarIds.xaml.cs
+++ ForzaMods_CarTable/Resources/CarIds.xaml.cs
@@ -14,24 +14,29 @@ namespace ForzaMods_CarTable.Resources
             InitializeComponent();
         }
 
+        // move the window
         private void Topbar_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
                 DragMove();
         }
 
+        // main scan thread
         private async void ScanForIDADdr_Click(object sender, RoutedEventArgs e)
         {
             MessageBox.Show("Before starting the scan you need to be in horizon promo and hovering over the fd viper, this wont continue until you click ok");
             Status.Content = "Status: Scanning for the ID Addr";
             string address = null;
 
+            // starts scan for id then waits till found
             await Task.Run(async () =>
             {
-                address = (await MainWindow.mw.M.AoBScan("BB 0B 00 00 AC 00 00 00 00 00 00 00 00 00 00 00 01", true, true, false)).FirstOrDefault().ToString("X");
+                address = (await MainWindow.mw.M.AoBScan("B8 0B 00 00 AC 00 00 00 00 00 ?? ?? 00 00 00 00", true, true, false)).FirstOrDefault().ToString("X");
             });
 
-            if (address != null)
+
+            // checks if address is null or 0, if not then makes a read thread
+            if (address != null || address != "0")
             {
                 Status.Content = "Status: Found the ID Addresses";
                 Thread readThread = new Thread(() =>
